e54d0525d76666cab33a726bf55fee18ad16c1dc
Matúš Makatura <matusmakapps@gmail.com>
2019-08-22 01:57:03 +0200

    Improved content's styling and added Open Sans font

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,10 @@
     </div>
     <router-view />
 -->
-  <SiteFooter />
+    <div class="app-content">
+      <router-view class="container" />
+    </div>
+    <SiteFooter />
   </div>
 
 </template>
@@ -27,24 +30,3 @@
 
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
