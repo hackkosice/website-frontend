72c870e72c515069ec39cccb3075b90cf062a0ee
Dominik Matis <domosino44@gmail.com>
2019-08-25 16:29:25 +0200

    Badge position and link fix

--- src/atoms/navbar/MlhBadge.vue
+++ src/atoms/navbar/MlhBadge.vue
@@ -2,7 +2,7 @@
   <div class="mlh-badge">
     <a
       id="mlh-trust-badge"
-      href="https://mlh.io/seasons/na-2020/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2020-season&utm_content=white"
+      href="https://mlh.io/seasons/eu-2020/events?utm_source=eu-hackathon&utm_medium=TrustBadge&utm_campaign=2020-season&utm_content=white"
       target="_blank"
       ><img
         src="https://s3.amazonaws.com/logged-assets/trust-badge/2020/mlh-trust-badge-2020-white.svg"
