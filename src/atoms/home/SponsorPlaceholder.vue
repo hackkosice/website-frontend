23f8dde8837ed789443615c9420117acee501b13
Matúš Makatura <matusmakapps@gmail.com>
2019-08-26 23:32:35 +0200

    Sponsors & Partners section component

---
+++ src/atoms/home/SponsorPlaceholder.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="sponsor d-flex h-100 w-100 justify-content-center text-center">
+    <router-link
+      class="d-flex w-100 align-items-center text-decoration-none"
+      to="/contact-us"
+    >
+      <span class="sponsor-placeholder w-100">Your company</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SponsorPlaceholder",
+
+  props: {
+    config: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
---
+++ src/atoms/home/SponsorTile.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="sponsor text-center d-flex h-100 justify-content-center">
+    <a
+      class="d-flex align-items-center"
+      :href="sponsor.url"
+      :title="sponsor.name"
+      target="_blank"
+    >
+      <img
+        class="img-fluid"
+        :class="sponsorClass"
+        :src="sponsor.logo"
+        :alt="sponsor.name"
+      />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SponsorTile",
+
+  props: {
+    config: {
+      type: Object,
+      required: true
+    },
+
+    sponsor: {
+      type: Object,
+      required: true
+    }
+  },
+
+  computed: {
+    sponsorClass: function() {
+      return `sponsor-logo sponsor-logo-${this.config.style}-${this.sponsor.style}`;
+    }
+  }
+};
+</script>
